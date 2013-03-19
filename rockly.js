66a8985d83c01a56d42a47ac2dde347bf9a25d9b
Avital Oliver <avital@thewe.net>
2013-03-19 13:35:46 +0200

    Seemingly working first insecure version

--- rockly.js
+++ rockly.js
@@ -1,7 +1,19 @@
 var Games = new Meteor.Collection("games");
 
+Meteor.methods({
+  play: function(gameId, move) {
+    var game = Games.findOne(gameId);
+
+    if (game.playerOne.name === Meteor.user().profile.name) {
+      Games.update(gameId, {$set: {"playerOne.move": move}});
+    } else if (game.playerTwo.name === Meteor.user().profile.name) {
+      Games.update(gameId, {$set: {"playerTwo.move": move}});
+    }
+  }
+});
+
 if (Meteor.isClient) {
-  Template.lobby.games = function(){
+  Template.lobby.games = function() {
     return Games.find();
   };
 
@@ -15,17 +27,51 @@ if (Meteor.isClient) {
     },
     'click .sithere': function(){
       if (Meteor.user()) {
-        var theGame = Games.findOne({_id: this._id});
-        if (theGame.playerOne.name === Meteor.user().profile.name) { // xcxc
-          alert("You can't play against yourself");
-        } else {
-          Games.update(this._id, {$set: {playerTwo: {name: Meteor.user().profile.name}}});
-        }
+        Games.update(this._id, {$set: {playerTwo: {name: Meteor.user().profile.name}}});
       } else {
         alert('please log in first');
       }
     }
   });
+
+  Template.game.canPlay = function () {
+    if (this.playerOne.name === Meteor.user().profile.name) {
+      return !this.playerOne.move;
+    } else if (this.playerTwo.name === Meteor.user().profile.name) {
+      return !this.playerTwo.move;
+    } else {
+      return false;
+    }
+  };
+
+  Template.game.possibleMoves = [{name: 'rock'}, {name: 'paper'}, {name: 'scissors'}];
+  Template.game.events({
+    'click': function (event, template) {
+      Meteor.call("play", template.data._id, this.name);
+    }
+  });
+
+  Template.game.canJoin = function () {
+    return this.playerOne.name !== Meteor.user().profile.name;
+  };
+
+  Template.game.gameOver = function () {
+    return this.playerOne.move && this.playerTwo.move;
+  };
+
+  Template.game.winner = function () {
+    var winners = {
+      rock: {paper: "playerTwo", scissors: "playerOne"},
+      paper: {rock: "playerOne", scissors: "playerTwo"},
+      scissors: {rock: "playerTwo", paper: "playerOne"}
+    };
+
+    var winner = winners[this.playerOne.move][this.playerTwo.move];
+    if (!winner)
+      return "TIE";
+    else
+      return this[winner].name;
+  };
 }
 
 if (Meteor.isServer) {
